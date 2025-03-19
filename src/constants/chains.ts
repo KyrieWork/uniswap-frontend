a5e7f3595b556a0094a1735d6438b8fa6ba46c72
kw <kyrieatwork@gmail.com>
2025-03-19 18:28:55 +0800

    feat: bsc testnet

---
+++ src/constants/chains.ts
@@ -0,0 +1,21 @@
+export enum SupportedChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GOERLI = 5,
+  KOVAN = 42,
+  ARBITRUM_ONE = 42161,
+  ARBITRUM_RINKEBY = 421611,
+  BSC_TESTNET = 97,
+}
+
+export const NETWORK_LABELS: { [chainId in SupportedChainId | number]: string } = {
+  [SupportedChainId.MAINNET]: 'Mainnet',
+  [SupportedChainId.RINKEBY]: 'Rinkeby',
+  [SupportedChainId.ROPSTEN]: 'Ropsten',
+  [SupportedChainId.GOERLI]: 'Görli',
+  [SupportedChainId.KOVAN]: 'Kovan',
+  [SupportedChainId.ARBITRUM_ONE]: 'Arbitrum',
+  [SupportedChainId.ARBITRUM_RINKEBY]: 'Arbitrum Testnet',
+  [SupportedChainId.BSC_TESTNET]: 'BNBChain Testnet',
+}
